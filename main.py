89e1740a424c89c27ac483ae19cd741f72d88df6
farm-art <farmart@protonmail.com>
2019-09-21 12:25:09 +0300

    add index and thank pages;

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ from dbmodels import *
 @app.route('/index')
 def index():
     kwargs = {'title': 'СТП им. Арташеса - Главная'}
-    return render_template('base.html', **kwargs)
+    return render_template('index.html', **kwargs)
 
 
 @app.route('/download/stencil')
 def download_stencil():
     return send_file('static/downloadable_content/ГОСТ 19.701-90v5.vssx',
                      attachment_filename='ГОСТ 19.701-90v5.vssx')
+
+
+@app.route('/thanks')
+def thank():
+    kwargs = {'title': 'СТП им. Арташеса - Главная'}
+    return render_template('thank.html', **kwargs)
 
 
 if __name__ == '__main__':
